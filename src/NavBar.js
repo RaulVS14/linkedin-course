77b8aff37b4039a68d7b00bc67b6e93410d349f6
raulm <raul.muser@stacc.ee>
2024-03-12 22:04:29 +0200

    Improve navigation styling

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,12 +1,14 @@
-import {Link} from "react-router-dom";
+import {NavLink, useLocation} from "react-router-dom";
 
 const NavBar = () => {
+    const {pathname} = useLocation();
+    console.log(pathname);
     return (
         <nav>
             <ul>
-                <li><Link to="/">Home</Link></li>
-                <li><Link to="/about">About</Link></li>
-                <li><Link to="/articles">Articles</Link></li>
+                <li><NavLink to="/" activeClassName="active">Home</NavLink></li>
+                <li><NavLink to="/about" activeClassName="active">About</NavLink></li>
+                <li><NavLink to="/articles" activeClassName="active">Articles</NavLink></li>
             </ul>
         </nav>
     )
