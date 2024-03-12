ae2940250a14475b0a99f40584f88cbf4e1aea77
raulm <raul.muser@stacc.ee>
2024-03-13 01:32:15 +0200

    Replace ArticlePage.js with data from database

--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -1,18 +1,32 @@
 import {useParams} from "react-router-dom";
-import articles from "../articles";
 import NotFoundPage from "./NotFoundPage";
+import {useState, useEffect} from "react";
+import axios from "axios";
 
 
 const ArticlePage = () => {
+    const [articleInfo, setArticleInfo] = useState({});
     const {articleId} = useParams();
-    const article = articles.find((article) => article.name === articleId);
-    if (!article) {
+    useEffect(() => {
+        const loadArticleInfo = async () => {
+            const response = await axios.get(`/api/articles/${articleId}`).catch((err)=>console.log(err));
+            const articleInfo = response?.data;
+            console.log(articleInfo);
+            setArticleInfo(articleInfo);
+        }
+        loadArticleInfo();
+    }, [articleId]);
+    if (!articleInfo) {
         return <NotFoundPage/>
+    }
+    if(Object.keys(articleInfo).length === 0){
+        return <></>
     }
     return (
         <>
-            <h1>{article.title}</h1>
-            {article.content.map((paragraph, idx) => <p key={idx}>{paragraph}</p>)}
+            <h1>{articleInfo.title}</h1>
+            <p>This article has {articleInfo.upvotes} upvote(s)</p>
+            {articleInfo.content.map((paragraph, idx) => <p key={idx}>{paragraph}</p>)}
         </>
     )
 }
