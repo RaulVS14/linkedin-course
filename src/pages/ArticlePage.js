d226871581dc01d5a80b54a07ca6d2a8be89506f
raulm <raul.muser@stacc.ee>
2024-03-13 01:42:10 +0200

    Add upvote functionality

--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -10,23 +10,31 @@ const ArticlePage = () => {
     const {articleId} = useParams();
     useEffect(() => {
         const loadArticleInfo = async () => {
-            const response = await axios.get(`/api/articles/${articleId}`).catch((err)=>console.log(err));
+            const response = await axios.get(`/api/articles/${articleId}`).catch((err) => console.log(err));
             const articleInfo = response?.data;
             console.log(articleInfo);
             setArticleInfo(articleInfo);
         }
         loadArticleInfo();
     }, [articleId]);
+    const addUpVote = async () => {
+        const response = await axios.put(`/api/articles/${articleId}/upvote`);
+        const updateArticle = response.data;
+        setArticleInfo(updateArticle);
+    }
     if (!articleInfo) {
         return <NotFoundPage/>
     }
-    if(Object.keys(articleInfo).length === 0){
+    if (Object.keys(articleInfo).length === 0) {
         return <></>
     }
     return (
         <>
             <h1>{articleInfo.title}</h1>
-            <p>This article has {articleInfo.upvotes} upvote(s)</p>
+            <div className="upvote-section">
+                <button onClick={addUpVote}>Upvote</button>
+                <p>This article has {articleInfo.upvotes} upvote(s)</p>
+            </div>
             {articleInfo.content.map((paragraph, idx) => <p key={idx}>{paragraph}</p>)}
             <CommentsList comments={articleInfo.comments}/>
         </>
