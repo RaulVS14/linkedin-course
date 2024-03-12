11a70dfbbd383af5d20e6365bfb2932c7e09476d
raulm <raul.muser@stacc.ee>
2024-03-12 21:38:03 +0200

    Add articles and set up single view

--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -1,6 +1,19 @@
+import {Navigate, useParams} from "react-router-dom";
+import articles from "../articles";
+
+
 const ArticlePage = () => {
+    const {articleId} = useParams();
+    const article = articles.find((article) => article.name === articleId);
+    if (!article) {
+        return <Navigate to="/404"/>
+    }
+    console.log(article);
     return (
-        <h1>This is the Article page!</h1>
+        <>
+            <h1>{article.title}</h1>
+            {article.content.map(paragraph => <p>{paragraph}</p>)}
+        </>
     )
 }
 export default ArticlePage;
