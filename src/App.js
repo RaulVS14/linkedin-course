11729db5f8325c1193bbea467e972e66f7ecbec6
Raul <raul.muser@gmail.com>
2024-03-12 19:05:27 +0200

    Add page routing

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
 import './App.css';
+import HomePage from "./pages/HomePage";
+import AboutPage from "./pages/AboutPage";
+import ArticlePage from "./pages/ArticlePage";
+import ArticlesListPage from "./pages/ArticlesListPage";
+import NotFoundPage from "./pages/NotFoundPage";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 
 function App() {
-  return (
-    <div className="App">
-      <h1>My blog page</h1>
-      <div id="page-body">
-          Welcome to the site
-      </div>
-    </div>
-  );
+    return (
+        <BrowserRouter>
+            <div className="App">
+                <h1>My blog page</h1>
+                <div id="page-body">
+                    <Routes>
+                        <Route path="/" element={<HomePage/>}/>
+                        <Route path="/about" element={<AboutPage/>}/>
+                        <Route path="/articles" element={<ArticlesListPage/>}/>
+                        <Route path="/articles/:articleId" element={<ArticlePage/>}/>
+                        <Route path="/404" element={<NotFoundPage/>}></Route>
+                    </Routes>
+                </div>
+            </div>
+        </BrowserRouter>
+    );
 }
 
 export default App;
