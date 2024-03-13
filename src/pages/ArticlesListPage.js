76020b730c5fd2d2f9b86470bcfb98322886a712
raulm <raul.muser@stacc.ee>
2024-03-13 02:03:30 +0200

    Add database data to ArticlesListPage.js

--- src/pages/ArticlesListPage.js
+++ src/pages/ArticlesListPage.js
@@ -1,12 +1,25 @@
-import articles from "../articles";
 import ArticlesList from "../components/ArticlesList";
+import {useEffect, useState} from "react";
+import axios from "axios";
 
 const ArticlesListPage = () => {
+    const [articles, setArticles] = useState([]);
+    useEffect(() => {
+        const loadArticles = async () => {
+            const response = await axios.get(`/api/articles`).catch((err) => console.log(err));
+            const articles = response?.data;
+            setArticles(articles);
+        }
+        loadArticles();
+    }, []);
+    if (!articles) {
+        return <></>
+    }
     return (
         <>
             <h1>Articles</h1>
             <ArticlesList articles={articles}/>
         </>
-    )
+    );
 }
 export default ArticlesListPage;
